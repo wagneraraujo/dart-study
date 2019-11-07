7b2fa9bdf38da40ea21bcc63a3f322ed04b1669c
Wagner Araújo <wagneraraujo15@gmail.com>
2019-11-07 09:46:47 -0300

    write class

--- dart02/subscritas.dart
+++ dart02/subscritas.dart
@@ -12,3 +12,60 @@
 
  *
  * */
+
+abstract class Animal{
+  String idade;
+  bool docil;
+  bool coluna;
+
+  Animal.vertebrados(this.idade, {this.docil}){
+    this.coluna = true;
+  }
+
+  Animal.invertebrados(this.idade, {this.docil}){
+    this.coluna = false;
+  }
+
+  void dormir(){
+    print('dorme como um animal');
+
+  }
+}
+
+
+abstract class Mamiferos extends Animal{
+  String sexo;
+  String desenvolvimento;
+
+
+  //construtores
+  Mamiferos.placentarios(this.sexo, idade, {docil}): super.vertebrados (idade, docil:docil){
+    this.desenvolvimento = 'Placentarios';
+  }
+  Mamiferos.marsupiais(this.sexo, idade, {docil}): super.vertebrados (idade, docil:docil){
+    this.desenvolvimento = 'Placentarios + bolsa externa';
+  }
+
+  Mamiferos.monotremados(this.sexo, idade, {docil}): super.vertebrados (idade, docil:docil){
+    this.desenvolvimento = 'ovo';
+  }
+
+  void alimentar(){
+    print('se alimenta');
+    print('como um');
+  }
+
+
+  class Cao extends Mamiferos{
+    String nome;
+    String raca;
+
+    Cao.domestico(this.nome, this.raca, String sexo, {String idade = 'indefinido', bool docil= true}) : super.placentarios(sexo, idade, docil:docil);
+  
+    Cao.selvagem(this.nome, this.raca, String sexo, {String idade = 'indefinido', bool docil= true}) : super.placentarios(sexo, idade, docil:docil);
+  }
+
+  void main(){
+    var cao = Cao.domestico('zoe', 'pug', 'femea');
+    print('nome: ${cao.nome} raca:${cao.sexo} idade${cao.idade} docil:${cao.docil}');
+  }
